4bf25985e4d1fbf33fe8395a711a55d6ee03bd91
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-06-20 22:08:32 +0900

    Postureモデルの修正

--- app/models/posture.rb
+++ app/models/posture.rb
@@ -1,7 +1,8 @@
 class Posture < ActiveHash::Base
   self.data = [
-    { id: 0, name: 'はい' },
-    { id: 1, name: 'いいえ' },
+    { id: 0, name: '' },
+    { id: 1, name: 'はい' },
+    { id: 2, name: 'いいえ' }
   ]
 
   include ActiveHash::Associations
