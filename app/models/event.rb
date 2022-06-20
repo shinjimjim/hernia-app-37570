66065d7f0775485cea73649c4425f112c1af3140
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-06-20 21:52:53 +0900

    アソシエーションの設定
    newファイルのActiveHash

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :pain
+  belongs_to :stretch
+  belongs_to :training
+  belongs_to :posture
+  belongs_to :meal
+  belongs_to :motion
+  belongs_to :smoking
 end
