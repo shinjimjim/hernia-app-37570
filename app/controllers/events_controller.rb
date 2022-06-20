fd84f6c59f0e6eeeb871b6d6f47e3407da7cc2fc
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-06-20 23:34:06 +0900

    eventsコントローラーの編集

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,15 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    Event.create(event_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:start_time, :pain_id, :stretch_if, :training_id, :posture_id, :meal_id, :motion_id, :smoking_id, :postscript)
+  end
 end
