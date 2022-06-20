51796515f2b0315cfe540805c583fc4ac8315b58
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-06-20 22:03:26 +0900

    Painモデルの修正

--- app/models/pain.rb
+++ app/models/pain.rb
@@ -1,15 +1,16 @@
 class Pain < ActiveHash::Base
   self.data = [
-    { id: 0, name: '0' },
-    { id: 1, name: '1' },
-    { id: 2, name: '2' },
-    { id: 3, name: '3' },
-    { id: 4, name: '4' },
-    { id: 5, name: '5' },
-    { id: 6, name: '6' },
-    { id: 7, name: '7' },
-    { id: 8, name: '8' },
-    { id: 9, name: '9' },
+    { id: 0, name: '' },
+    { id: 1, name: '0' },
+    { id: 2, name: '1' },
+    { id: 3, name: '2' },
+    { id: 4, name: '3' },
+    { id: 5, name: '4' },
+    { id: 6, name: '5' },
+    { id: 7, name: '6' },
+    { id: 8, name: '7' },
+    { id: 9, name: '8' },
+    { id: 10, name: '9' }
     { id: 10, name: '10' }
   ]
 
